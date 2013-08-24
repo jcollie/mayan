e86fb747e170f7aac5ad56020c73022cc953ea52
Jeffrey C. Ollie <jeff@ocjtech.us>
2013-08-24 10:03:19 -0500

    add paths

--- manage.py
+++ manage.py
@@ -5,6 +5,9 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mayan.settings")
 
+    sys.path.append(os.path.realpath('mayan/3rd_party_apps'))
+    sys.path.append(os.path.realpath('mayan/apps'))
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
